785946d1e8915619d622885b3ccf9a7125757947
vincingdi <110591050+vincingdi@users.noreply.github.com>
2026-04-08 19:19:05 +0300

    add youtube video summarizing functioality

--- videodown.py
+++ videodown.py
@@ -1,6 +1,6 @@
 import subprocess
 
 def download_video(url):
-    subprocess.run(["yt-dlp", "-t", "mp4", url, "-o", "video", "--write-subs", "--convert-subs", "srt", "--ffmpeg-location", r"C:\Users\Desktop-PC\AppData\Local\Microsoft\WinGet\Packages\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\ffmpeg-8.1-full_build\bin\ffmpeg.exe"])
+    subprocess.run(["yt-dlp", "-o", "video", "--ffmpeg-location", r"C:\Users\Desktop-PC\AppData\Local\Microsoft\WinGet\Packages\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\ffmpeg-8.1-full_build\bin\ffmpeg.exe", "-S", "ext:mp4:m4a", "--write-auto-subs", "--sub-langs", "en", "--convert-subs", "srt", url])
 
     
